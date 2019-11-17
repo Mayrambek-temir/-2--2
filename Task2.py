340fce34eed08ab9c2be3c06d1d7b40564366816
Mayrambek-temir <m.temirov2015@gmail.com>
2019-11-17 09:26:37 +0300

    task2

---
+++ Task2.py
@@ -0,0 +1,9 @@
+﻿N = float(input('Enter number of students: '))
+K = float(input('Enter number of apples: '))
+
+forStud=(K/N)
+student=str(forStud)
+forBasket=(K%N)
+basket=str(forBasket)
+print('Sum of apples for each student: '+ student)
+print('Sum of apples in basket: '+ basket )
